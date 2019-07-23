ccd2b309311f467672d9c68448014ab6a5d37948
Artyom <53091678+artkrblv@users.noreply.github.com>
2019-07-23 19:50:42 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { OrderModule } from 'ngx-order-pipe';
 import { AppComponent } from './app.component';
-import { ListComponent } from '.list/list.component';
+import { ListComponent } from './list/list.component';
 import { CardComponent } from './card/card.component';
 import { GridComponent } from './grid/grid.component';
 import { LockFilter } from './search.pipe';
